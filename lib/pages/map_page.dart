a57a3a703cb27c8cdcd70c8358cd69606b07db31
AlexisMettendorff <alexis.mettendorff@viacesi.fr>
2024-02-29 16:37:20 +0100

    Ajout géolocalisation

--- lib/pages/map_page.dart
+++ lib/pages/map_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
+import 'package:flutter_map_location_marker/flutter_map_location_marker.dart';
 import 'package:latlong2/latlong.dart';
 
 class MapPage extends StatelessWidget {
@@ -24,6 +25,7 @@ class MapPage extends StatelessWidget {
                     urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
                     userAgentPackageName: 'com.example.app',
                   ),
+                  CurrentLocationLayer(),
                   RichAttributionWidget(attributions: [TextSourceAttribution('Carte des tournois',
                   )])
                 ],
